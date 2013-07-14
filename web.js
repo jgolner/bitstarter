493b8268ed1e56e35f4ce120c684821c0d993d70
Jonathan Golner <ubuntu@ip-172-31-38-172.us-west-2.compute.internal>
2013-07-14 15:41:58 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var buffer = new Buffer(50);
 
+var content = fs.readFileSync ("index.html", "utf8"); 
+ 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log(buffer.toString());
 });
+  
