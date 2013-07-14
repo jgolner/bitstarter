0fdfd421a848b7cad7bf35d8839aadf37f1e50ca
Jonathan Golner <ubuntu@ip-172-31-38-172.us-west-2.compute.internal>
2013-07-14 16:10:51 +0000

    modified web.js a second time

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var content = fs.readFileSync ("index.html", "utf8"); 
+var buffer = new Buffer(550, "utf-8");
+var content = fs.readFileSync ("index.html", "utf-8"); 
  
-//var buffer = new Buffer(50, "utf8");
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf8"));
+  response.send(buffer.toString(content, "utf-8"));
 });
 
 var port = process.env.PORT || 5000;
