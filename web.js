a37cf19c406632e0269e0baa1991d7905deb9190
Jonathan Golner <ubuntu@ip-172-31-38-172.us-west-2.compute.internal>
2013-07-14 19:38:03 +0000

    RHopefully this works

--- web.js
+++ web.js
@@ -2,15 +2,16 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(28);
-var content = fs.readFileSync ("/home/ubuntu/hw4/node-js-sample/bitstarter/index.html" ); 
+var buffer = new Buffer(128);
+//var content = fs.readFileSync ("/home/ubuntu/hw4/node-js-sample/bitstarter/index.html" ); 
   
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(fs.readFileSync("./index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log(buffer.toString('ascii'));
+  //console.log(buffer.toString('ascii'));
+  console.log(fs.readFileSync("./index.html").toString());
 });
   
